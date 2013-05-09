2fff08e21ffa7406a5b4e356baa8ad27d90195a8
James Hunt <jameshunt@notonthehighstreet.com>
2013-05-09 12:09:25 +0100

    Add and remove people from the meeting

--- meeting.js
+++ meeting.js
@@ -6,15 +6,41 @@ function MeetingCtrl($scope) {
   $scope.elapsedTime = 0;
   $scope.started = false;
 
+  // The checkpoint stores the time and total whenever anything changes, eg the number of people.
+  // TODO: Can I store the checkpoint each time and only add the delta? Will that potentially "lose" seconds if the inteerval triggers in a different timeframe to clock time?
+  var checkpointTotal = 0;
+  var checkpointTime = null;
+
   $scope.startMeeting = function() {
     $scope.startTime = Date.now();
     $scope.started = true;
+    setCheckpoint();
     setInterval($scope.tick, 1000);
   }
 
+  $scope.incrementPeople = function() {
+    changePeople($scope.numberOfPeople + 1)
+  }
+
+  $scope.decrementPeople = function() {
+    changePeople($scope.numberOfPeople - 1)
+  }
+
   $scope.tick = function() {
-    $scope.elapsedTime = (Date.now() - $scope.startTime) / 1000;
-    $scope.totalCost = $scope.numberOfPeople * $scope.costPerPersonPerSecond * $scope.elapsedTime / 100;
+    var now = Date.now();
+    $scope.elapsedTime = (now - $scope.startTime) / 1000;
+    var checkpointElapsedTime = (Date.now() - checkpointTime) / 1000;
+    $scope.totalCost = checkpointTotal + $scope.numberOfPeople * $scope.costPerPersonPerSecond * checkpointElapsedTime / 100;
     $scope.$apply();
+  }
+
+  var changePeople = function(newNumberOfPeople) {
+    $scope.numberOfPeople = newNumberOfPeople;
+    setCheckpoint();
+  }
+
+  var setCheckpoint = function() {
+    checkpointTime = Date.now();
+    checkpointTotal = $scope.totalCost;
   }
 }
